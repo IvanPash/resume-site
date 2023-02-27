31b1521d35603c7e804e03d9e5d198640b9a29e4
Ivan Pashkov <iv.pashkov.2012@gmail.com>
2023-02-27 16:18:09 +0300

    Добавление mock данных для страницы English training

---
+++ mocks/english/data.js
@@ -0,0 +1,9 @@
+import A0 from './levels/A0'
+import A1 from './levels/A1'
+import A2 from './levels/A2'
+import B1 from './levels/B1'
+import B2 from './levels/B2'
+import C1 from './levels/C1'
+import C2 from './levels/C2'
+
+export default { A0, A1, A2, B1, B2, C1, C2 }
